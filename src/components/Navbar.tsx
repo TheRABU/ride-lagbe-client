38063b1a82ce5aeb94abe5a8370a34a5ecdb4ce8
TheRABU <mdfazlerabbi114@gmail.com>
2025-10-08 02:57:33 +0600

    working on landing page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,38 @@
-
-
 const Navbar = () => {
   return (
     <>
-        <div className="wrapper px-5 py-3">
-            <section className="bg-[#1E201E] flex justify-between items-center p-3 h-16 w-full text-white font-bold rounded-md shadow-md mx-auto">
-                <div className="logo">
-                    <h1 className="text-2xl font-bold">Ride Lagbe</h1>
+      <div className="wrapper px-1 lg:px-5 py-3">
+        <section className="bg-[#1E201E] flex justify-between items-center p-3 h-16 w-full text-white font-bold rounded-3xl shadow-md mx-auto">
+          <div className="logo">
+            <h1 className="text-2xl font-bold">Ride Lagbe</h1>
+          </div>
 
-                </div>
-                
-                 <div className="links flex-1 flex justify-center">
-                    <ul className="flex gap-8">
-                        <li className="hover:text-gray-300 cursor-pointer">Home</li>
-                        <li className="hover:text-gray-300 cursor-pointer">About</li>
-                        <li className="hover:text-gray-300 cursor-pointer">Contact</li>
-                    </ul>
-                    </div>
+          <div className="links flex-1 hidden lg:flex justify-center">
+            <ul className="flex gap-3">
+              <li className="hover:text-gray-300 cursor-pointer rounded-4xl px-4 py-2 bg-neutral-700">
+                Home
+              </li>
+              <li className="hover:text-gray-300 cursor-pointer rounded-4xl px-4 py-2 bg-neutral-700">
+                About
+              </li>
+              <li className="hover:text-gray-300 cursor-pointer rounded-4xl px-4 py-2 bg-neutral-700">
+                Contact
+              </li>
+            </ul>
+          </div>
 
-                <div className="buttons flex items-center gap-2">
-                        <button className="bg-[#FAF7F3] text-black px-3 py-1 rounded-md font-semibold hover:bg-gray-200">Login</button>
-                        <button className="bg-[#FAF7F3] text-black px-3 py-1 rounded-md font-semibold hover:bg-gray-200 ml-2">Sign Up</button>
-                </div>
-            </section>
-        </div>
-        
+          <div className="buttons flex items-center gap-1">
+            <button className="bg-[#FAF7F3] text-black px-4 py-2 rounded-4xl font-semibold hover:bg-gray-200">
+              Login
+            </button>
+            <button className="bg-[#FAF7F3] text-black px-4 py-2 rounded-4xl font-semibold hover:bg-gray-200 ml-2">
+              Sign Up
+            </button>
+          </div>
+        </section>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/homepage/Home.tsx
+++ src/pages/homepage/Home.tsx
@@ -1,11 +1,42 @@
-
+import bgImg from "../../assets/bg.jpg";
+import taxi from "../../assets/taxi.jpg";
 
 const Home = () => {
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    <div className="px-1">
+      <section className="rounded-t-4xl max-h-min lg:min-h-min mx-auto w-full bg-[#131010] flex items-center justify-center">
+        <section className="text-white body-font">
+          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 className="title-font text-4xl mb-4 font-medium text-white">
+                Book your ride now
+                <br className="hidden lg:inline-block" />
+                with Ride Lagbe
+              </h1>
+              <p className="mb-8 leading-relaxed">
+                Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+                air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+                park mlkshk tote bag selvage hot chicken authentic tumeric
+                truffaut hexagon try-hard chambray.
+              </p>
+              <div className="flex justify-center">
+                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                  Book Ride
+                </button>
+              </div>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <img
+                className="object-cover object-center rounded-md"
+                alt="hero"
+                src={taxi}
+              />
+            </div>
+          </div>
+        </section>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
